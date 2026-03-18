03feb5a869783494a8c81bfc8e2a6d269a4fdcd2
febri <soerjahartono22@gmail.com>
2026-03-18 21:20:45 +0700

    feat: adding spinn

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Page from "./layout/page";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Loading from "./components/ui/loading";
 
 const queryClient = new QueryClient();
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route>
-            <Route path="*" element={<Page />} />
-          </Route>
+          <Route path="*" element={<Page />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div>
+    <div className="fixed inset-0 flex items-center justify-center z-50">
       <img
         src="/assets/img/TransJakarta_Logo.png"
         className="w-20 animate-spin"
